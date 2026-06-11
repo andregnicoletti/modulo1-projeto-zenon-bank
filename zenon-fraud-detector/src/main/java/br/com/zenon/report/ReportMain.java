6bac0a93ccfed06c1b7f8935d89eea734ef32729
André Nicoletti <andregnicoletti@gmail.com>
2026-06-11 00:00:17 -0300

    Internacionalização do relatório

--- zenon-fraud-detector/src/main/java/br/com/zenon/report/ReportMain.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/report/ReportMain.java
@@ -1,19 +1,45 @@
 package br.com.zenon.report;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Currency;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class ReportMain {
 
     public static final String FILE = "data/ps_log.csv";
     public static final String FILE_ERROR = "data/error.csv";
 
-    void main() {
+    void main(String[] args) {
+
+        String language = args.length > 0 ? args[0] : "en";
+        var locale = Locale.of(language);
+
+        var integerFormatter = NumberFormat.getIntegerInstance(locale);
+        var currencyFormat = DecimalFormat.getCurrencyInstance(locale);
+        currencyFormat.setCurrency(Currency.getInstance("USD"));
+
+        var resourceBundle = ResourceBundle.getBundle("report", locale);
 
         var transactionReport = new TransactionReport();
         var statistic = transactionReport.generateReport(FILE);
+
+        String formatTotalTransactions = integerFormatter.format(statistic.totalTransactions());
+        String formatTotalFraud = integerFormatter.format(statistic.totalFrauds());
+        String formatTotalAmount = currencyFormat.format(statistic.totalAmount());
+
+        String msgTotalTransactions = resourceBundle.getString("label.total.transactions");
+        String msgTotalFrauds = resourceBundle.getString("label.total.frauds");
+        String msgTotalAmount = resourceBundle.getString("label.total.amount");
+
         IO.println("""
-                Total de linhas: %d
-                Total de fraudes: %d
-                Valor Total Transacionado: %.2f
-                """.formatted(statistic.totalTransactions(), statistic.totalFrauds(), statistic.totalAmount()));
+                %s: %s
+                %s: %s
+                %s: %s
+                """.formatted(msgTotalTransactions, formatTotalTransactions,
+                msgTotalFrauds, formatTotalFraud,
+                msgTotalAmount, formatTotalAmount));
 
     }
 
